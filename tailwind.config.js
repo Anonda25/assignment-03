26396c71a935dc5b6bd4912cd02533d143fcdd4a
Anonda25 <anondabiswas25@gmail.com>
2024-08-23 14:54:53 +0600

    added the lest push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,2 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./**/*.{html,js}"],
-    theme: {
-        extend: {
-            colors: {
-                'primary-1':'#ABEF5F'
-            },
-        },
-    },
-    plugins: [],
-}
 
 
